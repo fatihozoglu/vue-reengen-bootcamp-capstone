2a8e8d1125447a52f4096fdd4330bb6c9e3f5335
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-12-15 20:22:23 +0300

    feat: add request interceptor to httpClient for sending x-access-token in every request

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -5,8 +5,14 @@ const httpClient = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
     Accept: "application/json",
-    "x-access-token": authHeader(),
   },
 });
+
+const requestHandler = (request) => {
+  request.headers["x-access-token"] = authHeader();
+  return request;
+};
+
+httpClient.interceptors.request.use((request) => requestHandler(request));
 
 export default httpClient;
