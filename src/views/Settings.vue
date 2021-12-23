f6019f3c0e760bcbdc11466619387bbc5383bc6d
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-12-23 17:55:20 +0300

    feat: add user information to Settings.vue component

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="settings">
-    <h1>SETTINGS</h1>
+  <div v-if="user" class="settings">
+    <h3>User Information</h3>
+    <div class="input-field">
+      <label for="username">Username</label>
+      <div class="user-info" id="username">{{ user.username }}</div>
+    </div>
+    <div class="input-field">
+      <label for="email">Email</label>
+      <div class="user-info" id="email">{{ user.email }}</div>
+    </div>
+    <div class="input-field">
+      <label for="role">Role</label>
+      <div class="user-info" id="role">{{ user.role }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "Settings",
+  computed: {
+    ...mapState(["user"]),
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.settings {
+  padding: 50px 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: stretch;
+  gap: 30px;
+}
+.input-field {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 0 auto;
+  width: 300px;
+}
+.user-info {
+  font-weight: 600;
+  padding: 10px 20px;
+  border: 1px solid white;
+  border-radius: 4px;
+  background-color: white;
+  color: black;
+}
+</style>
